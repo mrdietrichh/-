a69a69cbf3c6e09b72b40ed38845718fde79a626
mrdietrichh <131181631+mrdietrichh@users.noreply.github.com>
2023-04-18 16:44:49 +0000

    class

--- fa/fafa.cpp
+++ fa/fafa.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class Person{
+private:
+int age = 20;
+public:
+void show(){
+    cout << this -> age;
+}
+void dr(){
+    age ++;
+}
+};
+
 int main(){
-    cout << "Hi";
-    cout << "Bye";
-    cout << "Haha";
+    Person Egor;
+
+    Egor.show();
 }
