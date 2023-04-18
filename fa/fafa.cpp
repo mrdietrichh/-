c62ba9ad8e03a99fced0c68913751a2477fa9da3
mrdietrichh <131181631+mrdietrichh@users.noreply.github.com>
2023-04-18 16:52:31 +0000

    func

--- fa/fafa.cpp
+++ fa/fafa.cpp
@@ -5,16 +5,20 @@ class Person{
 private:
 int age = 20;
 public:
+
 void show(){
     cout << this -> age;
 }
 void dr(){
     age ++;
 }
+
 };
 
 int main(){
     Person Egor;
 
+    Egor.show();
+    Egor.dr();
     Egor.show();
 }
