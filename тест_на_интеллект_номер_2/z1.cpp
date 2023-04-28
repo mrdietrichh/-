d4c0a76fe0272908eaa88d167933ad6f82d9b3bd
mrdietrichh <131181631+mrdietrichh@users.noreply.github.com>
2023-04-28 17:34:04 +0000

    gg

--- тест_на_интеллект_номер_2/z1.cpp
+++ тест_на_интеллект_номер_2/z1.cpp
@@ -13,23 +13,27 @@ int f2(int *mas, int kol){
     int sum = 0;
     int ma = mas[0];
     for (int i = 0; i < kol; i++){
-        max(ma, mas[i]);
+        ma = max(ma, mas[i]);
     }
     return ma;
 }
 
-int f3(int *mas1, int *mas2, int kol1, int kol2){
+void f3(int *mas1, int *mas2, int kol1, int kol2){
     int *mas3 = new int[kol1 + kol2];
     for (int i = 0; i < kol1; i++){
         mas3[i] = mas1[i];
     }
-    for (int i = kol1; i < kol2; i++){
-        mas3[i] = mas1[i - kol1];
+    for (int i = 0; i < kol2; i++){
+        mas3[i + kol1] = mas2[i];
     }
-    return *mas3;
+    
+    for (int i = 0; i < kol1 + kol2; i++){
+        cout << mas3[i] << " ";
+    }
+    delete[] mas3;
 }
 
-int f4(int **mat, int kol1, int kol2){
+int f4(int mat[5][4], int kol1, int kol2){
     int sum = 0;
     for (int i = 0; i < kol1; i++){
         for (int j = 0; j < kol2; j++){
@@ -39,8 +43,8 @@ int f4(int **mat, int kol1, int kol2){
     return sum;
 }
 
-int f5(int *mas, int kol){
-    int sum = 0;
+double f5(int *mas, int kol){
+    double sum = 0;
     int ma = mas[0];
     for (int i = 0; i < kol; i++){
         sum += mas[i];
@@ -55,11 +59,28 @@ int main(){
 
     for (int i = 0; i < 6; i++){
         mas1[i] = rand() % 10 + 1;
+        cout << mas1[i] << " ";
     }
+
+    cout << "\n";
 
     for (int i = 0; i < 8; i++){
         mas2[i] = rand() % 10 + 1;
+        cout << mas2[i] << " ";
     }
+
+    cout << "\n\n";
+
+    int mat[5][4];
+    for (int i = 0; i < 5; i++){
+        for (int j = 0; j < 4; j++){
+            mat[i][j] = rand() % 10 + 1;
+            cout << mat[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
     int n;
     cin >> n;
     cout << "\n";
@@ -71,7 +92,13 @@ int main(){
         cout << f2(mas1, 6);
         break;
         case 3:
-        cout << f3(mas1, mas2, 6, 8);
+        f3(mas1, mas2, 6, 8);
+        break;
+        case 4:
+        cout << f4(mat, 5, 4);
+        break;
+        case 5:
+        cout << f5(mas1, 6);
         break;
     }
 }
